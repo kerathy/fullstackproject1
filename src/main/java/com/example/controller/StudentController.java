51c2b431b6f031847f6c874a337c73f629d065c3
kerathy <xxyxuxiaoyan@126.com>
2021-04-29 08:43:09 -0700

    @value annotation

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/student")
 public class StudentController {
 
+//    @Value("${app.name}")
+    @Value("${app.name: Springboot Demo2}")
+    private String appName;
+
     @GetMapping("/get")
     public String getStudent() {
-        return "hello student";
+        return appName;
     }
 }
