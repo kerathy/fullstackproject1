6f2f3f6befa476eb9868d8bd7b6d17da6ee0e336
kerathy <xxyxuxiaoyan@126.com>
2021-04-29 08:34:27 -0700

    get api

--- src/main/java/com/example/springbootpracticedemo/SpringbootpracticedemoApplication.java
+++ src/main/java/com/example/springbootpracticedemo/SpringbootpracticedemoApplication.java
@@ -1,9 +1,11 @@
-package com.example.springbootpracticedemo.springbootpracticedemo;
+package com.example.springbootpracticedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.*")
 public class SpringbootpracticedemoApplication {
 
     public static void main(String[] args) {
