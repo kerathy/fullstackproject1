e38c64a5d1262c66a5ae2da5cb62ea1c17a439f9
kerathy <xxyxuxiaoyan@126.com>
2021-04-29 08:18:31 -0700

    springbootprojectsrelated

---
+++ src/main/java/com/example/springbootpracticedemo/springbootpracticedemo/SpringbootpracticedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpracticedemo.springbootpracticedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpracticedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootpracticedemoApplication.class, args);
+    }
+
+}
